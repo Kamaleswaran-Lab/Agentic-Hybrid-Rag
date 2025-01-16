44dfa24a111b997feed729e0f1b96b7e8a93f946
RicardoAccorsi <ric.accorsi@gmail.com>
2025-01-16 18:42:32 -0300

    Update get_database.py

--- get_database.py
+++ get_database.py
@@ -16,7 +16,11 @@ def format_data():
     artigos = [f.name for f in caminho_pasta.iterdir() if f.is_file() and f.name != ".ipynb_checkpoints"]
 
     chunks = []
+    
+    df_final = pd.DataFrame(columns=["doi", "title", "abstract", "year", "authors", "keywords", "introduction", "methods", "results", "conclusion"])
+    
     for artigo in tqdm(artigos, desc="Processing papers", unit="paper"):
+        
         pdf_path = f"../Papers/{artigo}"
 
         doi = artigo.split(".pd")[0]
@@ -51,7 +55,11 @@ def format_data():
         else:
             print(f"paper {doi} is missing the conclusion")
             conclusion = ""
-
+            
+        df_new = pd.DataFrame({"doi": doi, "title": df.loc[df.Doi == doi, "Title"].values[0], "abstract": df.loc[df.Doi == doi, "Abstract"].values[0], "year": df.loc[df.Doi == doi, "Year"].values[0], "authors": df.loc[df.Doi == doi, "Authors"].values[0], "keywords": df.loc[df.Doi == doi, "Keywords"].values[0], "introduction": intro, "methods": methods, "results": results, "conclusion": conclusion})
+        
+        df_final = pd.concat([df_final, df_new], ignore_index=True)
+        
         # get right format for each paper
         chunk = f'''
         Title: {df.loc[df.Doi == doi, "Title"].values[0]}
@@ -76,6 +84,8 @@ def format_data():
         '''
 
         chunks.append(chunk)
+    
+    df_final.to_excel("chunks.xlsx", index=False)
 
     print(chunks)
 
