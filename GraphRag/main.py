4748d326b07823f290175e2488a0a0cc5c33965b
Ricardo Accorsi <ric.accorsi@gmail.com>
2025-02-23 18:48:35 -0300

    Update main.py
    Added date range filter in main.py

--- GraphRag/main.py
+++ GraphRag/main.py
@@ -1,8 +1,8 @@
 import warnings
 
+from Functions.auxiliary import get_validated_input
 from Functions.search_papers import search_papers
 from Functions.select_papers import select_papers
-from Functions.auxiliary import get_validated_input
 from Functions.download_papers import download_papers
 
 
@@ -12,10 +12,15 @@ def main():
     warnings.filterwarnings("ignore")
 
     # Prompt user for necessary info
-    query, max_results, threshold = get_validated_input()
+    query, year_low, year_high, max_results, threshold = get_validated_input()
 
     # search papers in 3 different databases
-    papers = search_papers(query=query, max_results=max_results)
+    papers = search_papers(query=query, max_results=max_results, year_low=year_low, year_high=year_high)
+
+    # stop code if the query didn't retrieve any papers
+    if len(papers) == 0:
+        print("The search didn't retrieve any papers")
+        return
 
     # select papers after cleaning processes and data aquisition
     papers = select_papers(papers, query, threshold=threshold)
@@ -25,8 +30,8 @@ def main():
 
     # create graph
 
-
     # perform GraphRag
+
 
 if __name__ == "__main__":
     main()
